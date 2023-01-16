e55aace625d96e58db7cff736b7844b6802f96c4
Divyanshu Verma <dverma@Divyanshus-MacBook-Pro.local>
2023-01-16 09:55:41 -0500

    add redirect for root url to users index

--- users/urls.py
+++ users/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from django.urls import include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('userlist.urls'))
+    path('users/', include('userlist.urls')),
+    path('', lambda req: redirect('/users/')),
 ]
