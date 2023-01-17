b285b0436d4e71d7deca12b4dfe73eb978ca8302
Divyanshu Verma <divyanshu.verma1993@gmail.com>
2023-01-17 12:55:17 -0500

    uses ListView, CreateView, DeleteView and UpdateView and fixes failing tests

--- userlist/views.py
+++ userlist/views.py
@@ -1,59 +1,53 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from .models import AppUser
-from django.urls import reverse
-from django.views import View
+from django.urls import reverse_lazy
 from .forms.app_user_form import AppUserForm
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 # Create your views here.
 
-class UserListView(View):
+class UserListView(ListView):
     template_name = 'users.html'
-    def get(self, request):
-        users = AppUser.get_all_ordered_by_created_at()
-        context = {'users': users, 'heading': 'Team members'}
-        if len(users) == 1:
+    model = AppUser
+    ordering = '-created_at'
+    context_object_name = 'users'
+    extra_context = {'heading': 'Team members'}
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        users_length = len(context['users'])
+        if users_length == 1:
             context['sub_heading'] = 'You have 1 team member'
         else:
-            context ['sub_heading'] = 'You have ' + str(len(users)) + ' team members'
-        return render(request, self.template_name, context)
+            context ['sub_heading'] = 'You have ' + str(users_length) + ' team members'
+        return context
 
-class CreateUserView(View):
+class CreateUserView(CreateView):
     template_name = 'create_user_view.html'
     form_class = AppUserForm
-    context = {'heading': 'Add a team member', 'sub_heading': 'Set email, location and role', 'action': 'POST'}
-    def get(self, request):
-        context = { 'form': self.form_class(), **self.context}
-        return render(request, self.template_name, context)
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('index'))
-        return render(request, self.template_name, {'form': form, **self.context})
+    extra_context = {'heading': 'Add a team member', 'sub_heading': 'Set email, location and role', 'action': 'POST'}
+    success_url = reverse_lazy('index')
+    pk_url_kwarg = 'id'
+    http_method_names = ['get', 'post']
 
-
-class EditUserView(View):
+class EditUserView(UpdateView):
     template_name='edit_user_view.html'
-    context = {'heading': 'Edit a team member', 'sub_heading': 'Edit email, location and role', 'action': 'POST'}
+    extra_context = {'heading': 'Edit a team member', 'sub_heading': 'Edit email, location and role', 'action': 'POST'}
+    success_url = reverse_lazy('index')
     form_class = AppUserForm
-    def get(self, request, id):
-        app_user = get_object_or_404(AppUser, id=id)
-        form = self.form_class(instance=app_user)
-        context = {'form': form, 'user_id': id, **self.context }
-        return render(request, self.template_name, context)
-    
-    def post(self, request, id):
-        app_user = get_object_or_404(AppUser, id=id)
-        form = self.form_class(request.POST, instance=app_user)
-        if form.is_valid():
-            form.save();
-            return redirect(reverse('index'))
-        context = {'form': form, 'user_id': id, **self.context}
-        return render(request, self.template_name, context)
+    model = AppUser
+    pk_url_kwarg='id'
+    http_method_names = ['get', 'post']
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_id'] = self.kwargs['id']
+        return context
 
-class DeleteUserView(View):
-    def post(self, request, id):
-        app_user = get_object_or_404(AppUser, id=id)
-        app_user.delete();
-        return redirect(reverse('index'))
+class DeleteUserView(DeleteView):
+    pk_url_kwarg = 'id'
+    http_method_names=['post']
+    model= AppUser
+    pk_url_kwarg ='id'
+    success_url = '/users/'
+    http_method_names = ['post']
