d48fad400a1e7bbbe68a47e2a1844b2dc1543da9
Divyanshu Verma <divyanshu.verma1993@gmail.com>
2023-03-26 00:17:31 -0400

    configure postgres

--- users/settings.py
+++ users/settings.py
@@ -122,3 +122,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
+    }
+}
